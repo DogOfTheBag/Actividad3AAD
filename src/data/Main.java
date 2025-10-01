7727fa80f67a3839c897569bbb1312f0cf8295bc
DogOfTheBag <albercortijo@gmail.com>
2025-10-01 11:03:48 +0200

    me habia equivocado
    cambio el nombre del archivo

--- src/data/Main.java
+++ src/data/Main.java
@@ -4,13 +4,13 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
-        String ruta = "C:\\Users\\alber\\Documents\\IntelIJ Projects\\Actividad3AAD\\res\\ejemplo.txt";
+        String ruta = "C:\\Users\\alber\\Documents\\IntelIJ Projects\\Actividad3AAD\\res\\FichTexto.txt";
 
     //con esto aunque borres el archivo o no lo hagas antes, se hace siempre si no está
 
         try (PrintWriter writer = new PrintWriter(new FileWriter(ruta))) {
             System.out.println("ESCRIBIENDO POR PANTALLA...");
-            writer.write("Hola buenas noches \n");
+            writer.write("Hola buenas noches ");
             writer.write("Voy a ir a jugar al badminton");
             /*Se puede hacer con un try catch normal, si lo haces con el normal tienes que cerrar el writer porque si no
             * no guarda lo escrito en el archivo y no se verá nada
